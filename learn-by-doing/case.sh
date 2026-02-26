014c8875516ba38378a9d9529e0ba243c3e78377
Vaibhav <vaibhavnalla15@gmail.com>
2026-02-26 20:49:16 +0530

    Added case.sh

---
+++ learn-by-doing/case.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Please provide an option:- "
+echo "a for print date"
+echo "b for list scripts"
+echo "c to check the current location"
+echo "d for Jhon Wick's quotes"
+
+read -p "Enter your choice: " choice
+
+case $choice in 
+    a)date;;
+    b)ls;;
+    c)pwd;;
+    d)
+        echo -e "Behold, now you gonna see the best quotes from John Wick's all Chapters.\n"
+        cat /home/hunter/bash-scripting/learn-by-doing/wick_quotes.txt
+        ;;
+    *)echo "Plase enter a valid input"
+esac
+
+

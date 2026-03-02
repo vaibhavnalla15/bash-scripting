e9f3f9188c397da8a6337479292b865e1b6b63db
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-02 18:38:53 +0530

    Added ./infinite_for_loop.sh

---
+++ learn-by-doing/infinite_for_loop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for (( ;; ))
+do 
+    echo "Hi this is Jhon Wick."
+    sleep 2s
+done
---
+++ learn-by-doing/infinite_loop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+while true
+do 
+    echo "Hi this is Jhon Wick."
+    sleep 2s
+done

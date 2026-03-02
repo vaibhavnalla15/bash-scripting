2b8cdfba9861b260884a838f0a07f318a32f4f68
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-02 19:54:31 +0530

    Added ./while_with_csv.sh

--- learn-by-doing/while_with_csv.sh
+++ learn-by-doing/while_with_csv.sh
@@ -6,3 +6,12 @@ do
     echo "Name is $name"
     echo "Age is $age"
 done < /home/hunter/csv/test.csv 
+
+# or 
+
+cat /home/hunter/csv/test.csv  | awk 'awk NR!=1 {print}' | while IFS="," read -r id name age
+do
+    echo "Id is $id"
+    echo "Name is $name"
+    echo "Age is $age"
+done 

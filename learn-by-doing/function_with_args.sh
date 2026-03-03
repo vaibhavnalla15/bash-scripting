1025a696d2ab672dc8414c3f8b5576555b66cb85
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-03 18:15:27 +0530

    Added ./function_with_args.sh

---
+++ learn-by-doing/function_with_args.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+welcomeNote () {
+    echo -e "\n======================="
+    echo -e "\nWelcome $1"
+    echo -e "\nThey call me $2"
+    echo -e "\n======================="
+}
+
+# call the function:- 
+welcomeNote Jhon Wick BabaYaga

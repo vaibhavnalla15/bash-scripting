5bc3bc30064365e9f4ae9b1778b20cdce71276e1
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-03 18:02:51 +0530

    Added ./function_demo.sh

---
+++ learn-by-doing/function_demo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# creating a function:- 
+welcomeNote () {
+    echo -e "\n======================="
+    echo -e "\nWelcome Jhon Wick"
+    echo -e "\n======================="
+}
+
+# call the function:- 
+welcomeNote
+welcomeNote
+welcomeNote

a31fc4e7c23a4f88f7939fd590f626dcecab385f
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-04 13:03:30 +0530

    Added ./args.sh

---
+++ learn-by-doing/args.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# To access the arguments:- 
+
+echo "The First argument is:- $1"
+echo "The Second argument is:- $2"
+
+echo "All the arguments are:- $*"
+echo "Number of arguments are:- $#"
+
+

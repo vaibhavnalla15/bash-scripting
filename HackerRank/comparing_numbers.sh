f3ee29cbab3f6e11433c4987346ab008b0f1202e
Vaibhav <vaibhavnalla15@gmail.com>
2026-03-07 13:07:12 +0530

    Added ./comparing_numbers.sh

---
+++ HackerRank/comparing_numbers.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+read -r X
+read -r Y
+
+if [[ $X -lt $Y ]]; then
+    echo "X is less than Y"
+elif [[ $X -eq $Y ]]; then
+    echo "X is equal to Y"
+else
+    echo "X is greater than Y"
+fi
